2854bd0c89ab419218eb88e4eec86ba940dc68a9
gaurav0497 <51540604+gaurav0497@users.noreply.github.com>
2020-06-23 16:27:22 +0530

    nw chng

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,7 @@
 $(function (){
+	// equal to document.Queryselector(#elemet).blur
 	$("#navbarToggle").blur( function(event){
+
 		var screen = window.innerWidth;
 		if(screen<768){
 		$("#navbarSupportedContent").collapse('hide');
@@ -7,4 +9,11 @@ $(function (){
 
 	});
 
-	});
+	$("#navbarToggle").click(function (event) {
+    		$(event.target).focus();
+  });
+
+}); // for hiding toggle button when not in use 
+
+
+
